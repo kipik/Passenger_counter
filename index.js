49ff472a155e02543225d8a305690aed52875982
kipik <kipik.kipik@gmail.com>
2024-03-15 14:49:38 +0100

    Ajout du "Save" basique

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // initialise le compteur des entrées
 let count = 0;
-let countEl = document.getElementById("count-el");
+let saved = 0;
+const countEl = document.getElementById("count-el");
+const savedEl = document.getElementById("saved-el");
 
 // méthode = une fonction attachée à un objet
 // entre parenthèses : le pass in argument
@@ -13,5 +15,14 @@ function increment() {
   count++;
 
   // et modifie le HTML
+  countEl.innerText = count;
+}
+
+// Crée une fonction save() qui affiche le nombre d'entrées
+
+function save() {
+  saved = saved + count;
+  savedEl.innerText = saved;
+  count = 0;
   countEl.innerText = count;
 }
